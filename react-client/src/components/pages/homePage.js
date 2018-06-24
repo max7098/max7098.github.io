7a12931f4f05d711cc4efe5d182fe62910e67b94
maxCut <maxCut@github.com>
2018-06-24 03:09:46 -0700

    added community cast card

--- react-client/src/components/pages/homePage.js
+++ react-client/src/components/pages/homePage.js
@@ -6,16 +6,28 @@ class HomePage extends Component {
       <div className = "homePage">
         <h1>Projects</h1>
         <div className = "scrolling-wrapper">
-            <div className = "card">
+            <div className = "card" id = "pokemonInfinity">
                 <div className = "cardHeader">
                     <h2>Pokemon Infinity</h2>
                 </div>
                 <a href = "https://github.com/maxCut/pokemon_infinity">
-                    <div className = "cardContent" id = "pokemonInfinity">
+                    <div className = "cardContent">
                         <p>Procedural level mapping applied to "Pokemon" to create infinitely large worlds</p>
                     </div>
                 </a>
             </div>
+            <div className = "card" id = "communityCash">
+                <div className = "cardHeader">
+                    <h2>Community Cash</h2>
+                </div>
+                <a href = "https://github.com/Duck-and-Windmill/CommunitiCash">
+                    <div className = "cardContent">
+                        <p>Comunity Cash is a "micro-insurance" website that groups users together based on income and risk factors 
+                            Y-Hacks 2016 Intuit Mint Challenge Winner</p>
+                    </div>
+                </a>
+            </div>
+
             <div className = "card"><h2>Card</h2></div>
             <div className = "card"><h2>Card</h2></div>
             <div className = "card"><h2>Card</h2></div>
