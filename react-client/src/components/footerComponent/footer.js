ac068456eb1bd9712167a786b3ccb04abbd02b62
maxCut <maxCut@github.com>
2018-06-23 17:19:41 -0700

    added link

--- react-client/src/components/footerComponent/footer.js
+++ react-client/src/components/footerComponent/footer.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
+import GitLogo from '../../resources/GitHub-Mark-32px.png';
 
 class Footer extends Component {
   render() {
     return (
       <footer>
         <div>
-          Footer component
+            <div className = "links">
+                <a href = "https://github.com/maxCut">Github</a>
+                <a href = "mailto:max.mesirow@gmail.com">Email</a>
+            </div>
         </div>
       </footer>
     )
